30f648049461b89f8f7c9783d553509cd3cea8f7
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-08 12:37:58 +0300

    Add files via upload

---
+++ Рекурсия 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Рекурсия_1
+{
+    class Program
+    {
+
+        static double calc(int n, double x, int res = 1)
+        {
+            if (res > n) return x;
+            return x / (res + calc(n, x, res + 1));
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            double x;
+            do
+            {
+                try
+                {
+                    Console.Write("Введите n: ");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    if (n < 0) Console.WriteLine("Вы ввели n менье 0");
+                    else
+                    {
+                        Console.Write("Введите x: ");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            } while (true);
+
+            Console.WriteLine("Результат: " + calc(n, x));
+
+        }
+    }
+}
---
+++ Рекурсия 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Рекурсия_2
+{
+    class Program
+    {
+        static void Conv(string check, ref int num)
+        {
+            if (check != "")
+            {
+                if (check[0] == '1' | check[0] == '0')
+                {
+                    num = num * 2 + (check[0] == '1' ? 1 : 0);
+                    check = check.Substring(1);
+                    Conv(check, ref num);
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не двоичное");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Результат: " + num);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int num=0, c;
+            string check;
+            do
+            {
+                try
+                {
+                    Console.Write("Введите число для перевода: ");
+                    c = Convert.ToInt32(Console.ReadLine());
+                    check = Convert.ToString(c);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            } while (true);
+            Conv(check, ref num);
+
+        }
+    }
+}
